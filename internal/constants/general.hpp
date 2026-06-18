a5320a45d368bc343d0587236863a05e33aa2c39
Rafi Indra Pramudhito Zuhayr <zuudevs@gmail.com>
2026-06-18 07:26:37 +0700

    perf: major pipeline optimization via SWAR and devirtualization
    - Tokenizer: Replaced sequential literal scanning with compile-time SWAR constants (`std::bit_cast` to 32-bit uint) in `readAlphabet`, boosting Literal throughput to ~500M Tokens/s.
    - Parser: Devirtualized mutual recursion in `buildArray` and `buildObject` by inlining the `TokenType` switch-case, enabling aggressive compiler inlining.
    - Parser: Replaced `std::from_chars` with a naked fast-path loop in `buildInteger` for validated token streams.
    - Parser: Accelerated `unescapeString` using an 8-byte SWAR loop to bulk-copy normal text without branching.
    - Parser: Implemented a branchless bitwise algorithm for `decodeUnicodeHex` to eliminate branch mispredictions.
    - Refactor: Extracted safe-path integer parsing into a standalone `parse_int.hpp` trait for the public wrapper API.

--- internal/constants/general.hpp
+++ internal/constants/general.hpp
@@ -14,5 +14,10 @@ namespace zuu::constants {
 
 constexpr unsigned char digit = 10;
 constexpr unsigned char byte = 8;
+constexpr unsigned char word = 16;
+constexpr unsigned char hex_alpha_max_val = 15;
+constexpr unsigned char hex_alpha_min_val = 10;
+constexpr unsigned char hex_digit_max_val = 9;
+constexpr unsigned char hex_digit_min_val = 0;
 
 } // namespace zuu::constants
---
+++ internal/utils/parser.hpp
@@ -0,0 +1,30 @@
+/**
+ * @file parser.hpp
+ * @author zuudevs (zuudevs@gmail.com)
+ * @brief Brief description
+ * @version 0.1.0
+ * @date 2026-06-18
+ * 
+ * @copyright Copyright (c) 2026
+ */
+
+#pragma once
+
+#include "utils/parse_double.hpp"
+#include "utils/parse_int.hpp"
+
+namespace zuu::utils {
+
+template <typename T>
+[[nodiscard]] inline constexpr auto 
+parse(const char* first, const char* last) noexcept {
+	return traits::ParserTrait<T>{}(first, last);
+}
+
+template <typename T>
+[[nodiscard]] inline constexpr auto 
+parse(std::string_view value) noexcept {
+	return traits::ParserTrait<T>{}(value.data(), value.data() + value.size());
+}
+
+} // namespace zuu::utils
